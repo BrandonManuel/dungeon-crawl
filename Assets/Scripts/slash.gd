f4a3972ee86c82b0bed2e4d8116f329e4fe6a910
Brandon Manuel <brandon-manuel@live.com>
2026-07-12 14:05:10 -0500

    Improved slash to not stay out as long as a dangling hitbox. Slash also trails sword rather than sticking to it

---
+++ Assets/Scripts/slash.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var is_frozen: bool = false
+var freeze_location: Vector2
+
+func _physics_process(delta: float) -> void:
+	if is_frozen:
+		global_position = freeze_location
+		print('frozen at: ', freeze_location)
+
+
+func freeze(position: Vector2) -> void:
+	is_frozen = true
+	freeze_location = position
--- Assets/Scripts/sword.gd
+++ Assets/Scripts/sword.gd
@@ -2,15 +2,38 @@ extends Weapon
 
 @onready var collision_shape_2d: CollisionShape2D = $Effect/Slash/Area2D/CollisionShape2D
 @onready var slash: Sprite2D = $Effect/Slash
+@onready var effect: Node2D = $Effect
 
 @export var knockback: float
 
-var is_attacking: bool = false
+var slash_global_position: Vector2
+var slash_local_position: Vector2
+
+var slash_global_rotation: float
+var slash_local_rotation: float
+
+var slash_location_set: bool = false
+
 func _physics_process(delta: float) -> void:
 	if slash.visible:
+		if !slash_location_set:
+			slash_location_set = true
+			slash_global_position = effect.global_position
+			slash_local_position = effect.position
+			slash_global_rotation = effect.global_rotation
+			
+			effect.top_level = true
+			effect.global_position = slash_global_position
+			effect.global_rotation = slash_global_rotation
+			
 		collision_shape_2d.disabled = false
 	else:
-		collision_shape_2d.disabled = true
+		if slash_location_set:
+			slash_location_set = false
+			effect.top_level = false
+			effect.position = slash_local_position
+			effect.rotation = slash_local_rotation
+			collision_shape_2d.disabled = true
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
